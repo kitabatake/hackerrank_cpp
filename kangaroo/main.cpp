fbe1d7723a964be6c6a1f9ea723186adc474dfc9
Tatsuya Kitabatake <e2tod2@gmail.com>
2018-01-18 12:26:13 +0900

    kangaroo class

--- kangaroo/main.cpp
+++ kangaroo/main.cpp
@@ -4,23 +4,31 @@
 
 using namespace std;
 
-struct kangaroo {
+class kangaroo {
+public:
     int x;
     int v;
+    void move() {
+        x += v;
+    }
+    void print() {
+        cout << "x:" << x  << ", v:" << v << endl;
+    }
+    bool operator==(kangaroo k1) {
+        return x == k1.x;
+    }
 };
-
-ostream &operator << (ostream &os, kangaroo const &r) {
-    return os << r.x << ":" << r.v;
-}
 
 
 bool judge(kangaroo faster, kangaroo slower) {
+    faster.print();
+    slower.print();
     if (faster.x > slower.x) return false;
 
     while(faster.x <= slower.x) {
-        if (faster.x == slower.x) return true;
-        faster.x += faster.v;
-        slower.x += slower.v;
+        if (faster == slower) return true;
+        faster.move();
+        slower.move();
     }
     return false;
 }
@@ -29,7 +37,7 @@ int main () {
     int x1, x2, v1, v2;
     cin >> x1 >> v1 >> x2 >> v2;
 
-    struct kangaroo k1 = {x1, v1}, k2 = {x2, v2}, faster, slower;
+    kangaroo k1 = {x1, v1}, k2 = {x2, v2}, faster, slower;
     if (k1.v >= k2.v) {
         faster = k1;
         slower = k2;
